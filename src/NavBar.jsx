000821ecc732a5f5cdb23ca30f39f09b9c8ec550
Kelar Crisp and Na Hyung Choi <kelar@>
2019-09-04 15:55:32 -0700

    add navbar functionality

--- src/NavBar.jsx
+++ src/NavBar.jsx
@@ -1,24 +1,47 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import instaLogo from './assets/images/instaLogo.png';
-
+import image1 from './assets/images/image1.png';
+import image2 from './assets/images/image2.png';
+import image3 from './assets/images/image3.png';
 function NavBar() {
 
 
 
 
-  var usernameStyles = {
-    fontFamily: "sans-serif",
-    fontColor: "black",
+  var logoStyles = {
+    height: "45px"
+  }
+  var search = {
+    height: "25px",
+    width: "150px",
+    marginRight: "200px",
+    marginLeft: "200px",
+    marginBottom: "20px"
 
   }
 
+  var bottomBorder = {
+    borderBottomStyle: "solid",
+    borderBottomWidth: "1px",
+    borderBottomColor: "black"
+  }
+
   return (
-    <div >
+    <div style={bottomBorder}>
 
-      <img src={instaLogo} />
 
-    </div>
+      <Link to="/"> <img style={logoStyles} src={instaLogo} /></Link>
+      <input style={search}
+        type="text"
+        placeholder="Search" />
+      <img src={image1} />
+      <img src={image2} />
+      <img src={image3} />
+
+
+    </div >
+
   )
 }
 
